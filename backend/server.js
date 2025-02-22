38023f7caa115d9d9bca57e5211348c0834c31da
zhengfu200 <luguanyu08222@gmail.com>
2025-02-22 20:27:02 +0800

    🦘 2025/2/22:后端加入顶部bar生成

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(svgDirectory)) {
 function generateSymbolElements(text, DelayTime, DelayId, typingSpeed) {
     const totalCharacters = text.length;
     const freezeTime = `${totalCharacters * typingSpeed + DelayTime}s`;
-    return `<tspan id="char${DelayId}" visibility="hidden" font-size="8" fill="#ECF0F1" x="10">>&#160;&#160;</tspan>
+    return `<tspan id="char${DelayId}" visibility="hidden" fill="#ECF0F1" x="10">>&#160;&#160;</tspan>
             <animate href="#char${DelayId}" attributeName="visibility" from="hidden" to="visible" begin="${DelayTime}s" dur="${typingSpeed}s" fill="freeze" />
             <animate href="#char${DelayId}" attributeName="opacity" from="0" to="1" dur="${typingSpeed}s" begin="${DelayTime}s" repeatCount="indefinite" />
             <animate href="#char${DelayId}" attributeName="opacity" from="1" to="1" begin="${freezeTime}" dur="0s" fill="freeze" />`
@@ -22,17 +22,25 @@ function generateSymbolElements(text, DelayTime, DelayId, typingSpeed) {
 
 function generateTspanElements(text, DelayTime, DelayId, typingSpeed) {
     return text.split('').map((char, index) => {
-        return `<tspan id="char${DelayId+ index + 1}" visibility="hidden">${char === ' ' ? '&#160;' : char}</tspan>
-        <animate href="#char${DelayId+ index + 1}" attributeName="visibility" from="hidden" to="visible" begin="${index * typingSpeed + DelayTime}s" dur="${typingSpeed}s" fill="freeze"/>`;
+        return `<tspan id="char${DelayId + index + 1}" visibility="hidden">${char === ' ' ? '&#160;' : char}</tspan>
+        <animate href="#char${DelayId + index + 1}" attributeName="visibility" from="hidden" to="visible" begin="${index * typingSpeed + DelayTime}s" dur="${typingSpeed}s" fill="freeze"/>`;
     }).join('\n');
 }
 
 function generateBackgroundElements(background_height) {
     console.log("背景高度", background_height);
-    return `<rect width="100%" height="${background_height}px" fill="#2C3E50" rx="5"/>`;
+    return `<rect width="100%" height="${background_height}px" fill="#2C3E50" rx="5" y="30"/>`;
 }
 
-function generateTextElements(text, typingSpeed){
+function generateBarTitle(title, totalLength, typingSpeed) {
+    return `<rect width="100%" height="30" fill="black" rx="5" y="0" />
+            <rect height="2" fill="#bdc3c7" rx="5" y="0">
+                <animate attributeName="width" from="0" to="100%" dur="${totalLength*typingSpeed}s" fill="freeze" />
+            </rect>
+            <text text-anchor="middle" x="50%" fill="white" y="20" font-size="">${title}</text>`
+}
+
+function generateTextElements(text, typingSpeed) {
     let textElements = '';
     let DelayTime = 0;
     let DelayId = 0;
@@ -41,9 +49,9 @@ function generateTextElements(text, typingSpeed){
         const generateTspanElements_content = generateTspanElements(text, DelayTime, DelayId, typingSpeed);
         console.log(`Index: ${index}, Content: ${text}, length: ${text.length}, DelayTime: ${DelayTime}`);
         DelayTime = DelayTime + text.length * typingSpeed;
-        DelayId = DelayId + text.length +1;
+        DelayId = DelayId + text.length + 1;
         textElements += `
-        <text x="10" y="${index*10 + 20}" font-family="Arial" font-size="8" fill="#ECF0F1" letter-spacing="-0.5">
+        <text x="10" y="${index * 20 + 50}" font-family="Arial" fill="#ECF0F1" letter-spacing="-0.5">
             ${generateSymbolElements_content}
             ${generateTspanElements_content}
         </text>`
@@ -51,14 +59,17 @@ function generateTextElements(text, typingSpeed){
     return textElements;
 }
 
-function generateSvgCode(text, typingSpeed){
-    const background_height = text.length*10 + 20;
+function generateSvgCode(text, typingSpeed, title) {
+    const totalLength = text.reduce((sum, currentText) => sum + currentText.length, 0);
+    const background_height = text.length * 20 + 20;
     const randomFileName = `${Math.floor(Math.random() * 1000000)}.svg`;
     const filePath = path.join(svgDirectory, randomFileName);
     const backgroundElements = generateBackgroundElements(background_height);
+    const generateBarTitle_content = generateBarTitle(title, totalLength, typingSpeed);
     const textElements = generateTextElements(text, typingSpeed);
     const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+    <svg xmlns="http://www.w3.org/2000/svg" width="500" height="300">
+            ${generateBarTitle_content}
             ${backgroundElements}
             ${textElements}
     </svg>`;
@@ -69,14 +80,14 @@ function generateSvgCode(text, typingSpeed){
 app.post('/submit-text', (req, res) => {
     const text = ["Hello World", "Hi im zhengfu200"];
     const typingSpeed = 0.1;
-
+    const title = "My Code";
     if (!text) {
         return res.status(400).json({ error: 'Text is required' });
     }
 
     console.log("received text: ", text);
 
-    generateSvgCode(text, typingSpeed);
+    generateSvgCode(text, typingSpeed, title);
 
     return res.json({ message: '生成结束' });
 
