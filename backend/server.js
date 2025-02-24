92ae52a5a5acd5c24f8b7c3906c9a055806ad0a9
zhengfu200 <luguanyu08222@gmail.com>
2025-02-24 23:19:27 +0800

    🪵 2025/2/24:后端增加自适应svg高度宽度

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { get } = require('http');
 const path = require('path');
 const app = express();;
 const port = 3000;
@@ -9,6 +10,15 @@ app.use(express.json());
 const svgDirectory = path.join(__dirname, 'svg');
 if (!fs.existsSync(svgDirectory)) {
     fs.mkdirSync(svgDirectory);
+}
+
+function getSvgWidth(text){
+    const longestText = text.reduce((longest, current) => {
+        return current.length > longest.length ? current : longest;
+    }, "");
+
+    const background_width = (longestText.length + 2 )* 11 + 20;
+    return background_width;
 }
 
 function generateSymbolElements(text, DelayTime, DelayId, typingSpeed) {
@@ -29,11 +39,11 @@ function generateTspanElements(text, DelayTime, DelayId, typingSpeed) {
 
 function generateBackgroundElements(background_height) {
     console.log("背景高度", background_height);
-    return `<rect width="100%" height="${background_height}px" fill="#2C3E50" rx="5" y="30"/>`;
+    return `<rect width="100%" height="${background_height}px" fill="#2C3E50" rx="0" ry="5" y="30"/>`;
 }
 
 function generateBarTitle(title, totalLength, typingSpeed) {
-    return `<rect width="100%" height="30" fill="black" rx="5" y="0" />
+    return `<rect width="100%" height="30" fill="black" rx="5" ry="0" y="0" />
             <rect height="2" fill="#bdc3c7" rx="5" y="0">
                 <animate attributeName="width" from="0" to="100%" dur="${totalLength*typingSpeed}s" fill="freeze" />
             </rect>
@@ -62,19 +72,19 @@ function generateTextElements(text, typingSpeed) {
 function generateSvgCode(text, typingSpeed, title) {
     const totalLength = text.reduce((sum, currentText) => sum + currentText.length, 0);
     const background_height = text.length * 20 + 20;
+    const background_width = getSvgWidth(text);
     const randomFileName = `${Math.floor(Math.random() * 1000000)}.svg`;
     const filePath = path.join(svgDirectory, randomFileName);
     const backgroundElements = generateBackgroundElements(background_height);
     const generateBarTitle_content = generateBarTitle(title, totalLength, typingSpeed);
     const textElements = generateTextElements(text, typingSpeed);
     const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="500" height="300">
+    <svg xmlns="http://www.w3.org/2000/svg" width="${background_width}" height="${background_height +30}">
             ${generateBarTitle_content}
             ${backgroundElements}
             ${textElements}
     </svg>`;
-
-    console.log("内容：" + svgContent);
+    return svgContent;
 }
 
 app.post('/submit-text', (req, res) => {
@@ -87,9 +97,28 @@ app.post('/submit-text', (req, res) => {
 
     console.log("received text: ", text);
 
-    generateSvgCode(text, typingSpeed, title);
+    svgContent = generateSvgCode(text, typingSpeed, title);
+    console.log("内容：" + svgContent);
 
-    return res.json({ message: '生成结束' });
+
+    const filename = Math.floor(100000 + Math.random() * 900000).toString();
+    const dirPath = path.join(__dirname, 'svg');
+
+    try {
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true });
+        }
+        const filePath = path.join(dirPath, `${filename}.svg`);
+        fs.writeFileSync(filePath, svgContent);
+        
+        return res.json({ 
+            message: '生成结束',
+            filename: `${filename}.svg`
+        });
+    } catch (err) {
+        console.error('文件写入失败:', err);
+        return res.status(500).json({ error: '服务器文件存储错误' });
+    }
 
 })
 
